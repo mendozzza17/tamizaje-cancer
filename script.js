e436811946bf973531c833647f9cf2af7e2c7d83
mendozzza17 <mendozarocha.moisesdavid17@gmail.com>
2025-07-23 11:27:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function irAPagina(id) {
+  document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+  window.scrollTo(0, 0);
+}
