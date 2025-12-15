de4f136d4ae3675f6c109efb756064d556509a2d
mendozzza17 <mendozarocha.moisesdavid17@gmail.com>
2025-12-15 17:35:53 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function irAPagina(idPagina) {
-  const paginas = document.querySelectorAll('.page');
-  paginas.forEach(p => p.classList.remove('active'));
-  document.getElementById(idPagina).classList.add('active');
-}
